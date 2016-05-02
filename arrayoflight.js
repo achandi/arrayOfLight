7daacc93cf8a85a169c76aae495a734c368d23c7
achandi <chandi.ajit@gmail.com>
2016-05-02 12:28:13 -0400

    Array of light

---
+++ arrayoflight.js
@@ -0,0 +1,11 @@
+var arrayOfLight = function(number) {
+    array = [];
+    for(var i = 0; i <= number; i++) {
+      array.push(i);
+    }
+     
+      console.log(array);
+};
+
+arrayOfLight(3);
+
